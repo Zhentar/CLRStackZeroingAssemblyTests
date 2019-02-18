0b41acd97054c06b247ec201c34bf5bb2724002b
Zhentar <zhentar@gmail.com>
2019-02-17 23:50:32 -0600

    Use the Instruction.Create stuff I somehow overlooked

--- AssemblyTests/Assembler.cs
+++ AssemblyTests/Assembler.cs
@@ -7,7 +7,6 @@ namespace AssemblyTests
 {
 	public partial class Assembler
 	{
-		[SuppressUnmanagedCodeSecurity]
 		public delegate void BasicActionDelegate();
 
 		private readonly ExecutableCodeWriter _writer = new ExecutableCodeWriter();
@@ -15,8 +14,7 @@ namespace AssemblyTests
 		public T Compile<T>(IList<Instruction> instructions) where T : Delegate
 		{
 			var block = new InstructionBlock(_writer, instructions, _writer.NextFunctionPointer);
-			var bitness = Environment.Is64BitProcess ? 64 : 32;
-			if (!BlockEncoder.TryEncode(bitness, block, out var errorMessage))
+			if (!BlockEncoder.TryEncode(IntPtr.Size * 8, block, out var errorMessage))
 			{
 				throw new InvalidOperationException(errorMessage);
 			}
@@ -26,173 +24,74 @@ namespace AssemblyTests
 
 		public void Compile<T>(ref T @delegate, IEnumerable<Instruction> instructions) where T : Delegate
 		{
-			var instructionList = new InstructionList(instructions)
-			{
-				new Instruction() { Code = Code.Retnq }
-			};
-
+			var instructionList = new InstructionList(instructions) { Instruction.Create(Code.Retnq) };
 			@delegate = Compile<T>(instructionList);
 		}
 
-
-
 		public static IEnumerable<Instruction> SpillRegister(Register reg, IEnumerable<Instruction> containedInstructions)
 		{
-			yield return new Instruction() { Code = Code.Push_r64, Op0Register = reg };
-
-			foreach (var inst in containedInstructions)
-			{
-				yield return inst;
-			}
-
-			yield return new Instruction() { Code = Code.Pop_r64, Op0Register = reg };
+			yield return Instruction.Create(Code.Push_r64, reg);
+			foreach (var inst in containedInstructions) { yield return inst; }
+			yield return Instruction.Create(Code.Pop_r64, reg);
 		}
 
 		public static IEnumerable<Instruction> IncrementStack(uint bytes, IEnumerable<Instruction> containedInstructions)
 		{
-			yield return new Instruction()
-			{
-				Code = Code.Sub_rm64_imm32,
-				Op0Register = Register.RSP,
-				Op1Kind = OpKind.Immediate32to64,
-				Immediate32 = bytes
-			};
-
-			foreach (var inst in containedInstructions)
-			{
-				yield return inst;
-			}
-
-			yield return new Instruction()
-			{
-				Code = Code.Add_rm64_imm32,
-				Op0Register = Register.RSP,
-				Op1Kind = OpKind.Immediate32to64,
-				Immediate32 = bytes
-			};
+			yield return Instruction.Create(Code.Sub_rm64_imm32, Register.RSP, bytes);
+			foreach (var inst in containedInstructions) { yield return inst; }
+			yield return Instruction.Create(Code.Add_rm64_imm32, Register.RSP, bytes);
 		}
 
 		public static IEnumerable<Instruction> SaveRcxToANonVolatileRegister(IEnumerable<Instruction> containedInstructions)
 		{
-			//mov     rsi,rcx
-			yield return new Instruction()
-			{
-				Code = Code.Mov_r64_rm64,
-				Op0Register = Register.RSI,
-				Op1Register = Register.RCX
-			};
-
-			foreach (var inst in containedInstructions)
-			{
-				yield return inst;
-			}
-
-			//mov     rcx,rsi
-			yield return new Instruction()
-			{
-				Code = Code.Mov_r64_rm64,
-				Op0Register = Register.RCX,
-				Op1Register = Register.RSI
-			};
+			yield return Instruction.Create(Code.Mov_r64_rm64, Register.RSI, Register.RCX);
+			foreach (var inst in containedInstructions) { yield return inst; }
+			yield return Instruction.Create(Code.Mov_r64_rm64, Register.RCX, Register.RSI);
 		}
 
 		public static IEnumerable<Instruction> ZeroStackSSE2(uint stackBytes, byte stackOffset)
 		{
-			yield return new Instruction() {Code = Code.VEX_Vzeroupper};
-
-			//lea     r10,[rsp+28h]
-			yield return new Instruction()
-			{
-				Code = Code.Lea_r64_m,
-				Op0Register = Register.R10,
-				Op1Kind = OpKind.Memory,
-				MemoryBase = Register.RSP,
-				MemoryDisplSize = 0x1,
-				MemoryDisplacement = stackOffset
-			};
-
-			yield return new Instruction()
-			{
-				Code = Code.VEX_Vxorps_xmm_xmm_xmmm128,
-				Op0Register = Register.XMM0,
-				Op1Register = Register.XMM0,
-				Op2Register = Register.XMM0
-			};
-
 			if (!((stackBytes & 0xF) == 0)) { throw new NotImplementedException(); }
 
-			for (uint i = 0; i < stackBytes; i+= 0x10)
+			yield return Instruction.Create(Code.VEX_Vzeroupper);
+			yield return Instruction.Create(Code.VEX_Vxorps_xmm_xmm_xmmm128, Register.XMM0, Register.XMM0, Register.XMM0);
+			
+			for (int i = 0; i < stackBytes; i+= 0x10)
 			{
-				yield return new Instruction()
-				{
-					Code = Code.VEX_Vmovdqu_xmmm128_xmm,
-					Op0Kind = OpKind.Memory,
-					Op1Register = Register.XMM0,
-					MemoryBase = Register.R10,
-					MemoryDisplSize = 0x1,
-					MemoryDisplacement = i
-				};
+				var memory = new MemoryOperand(Register.RSP, i + stackOffset);
+				yield return Instruction.Create(Code.VEX_Vmovdqu_xmmm128_xmm, memory, Register.XMM0);
 			}
-
 		}
-
 
 		public static IEnumerable<Instruction> ZeroStack(uint stackBytes, byte stackOffset, byte operandSize)
 		{
 			//lea     rdi,[rsp+28h]
-			yield return new Instruction()
-			{
-				Code = Code.Lea_r64_m,
-				Op0Register = Register.RDI,
-				Op1Kind = OpKind.Memory,
-				MemoryBase = Register.RSP,
-				MemoryDisplSize = 0x1,
-				MemoryDisplacement = stackOffset
-			};
+			var stackOp = new MemoryOperand(Register.RSP, stackOffset);
+			yield return Instruction.Create(Code.Lea_r64_m, Register.RDI, stackOp);
 
 			//mov     ecx,20h
-			yield return new Instruction()
-			{
-				Code = Code.Mov_r32_imm32,
-				Op0Register = Register.ECX,
-				Op1Kind = OpKind.Immediate32,
-				Immediate32 = stackBytes / operandSize
-			};
-
+			yield return Instruction.Create(Code.Mov_r32_imm32, Register.ECX, stackBytes / operandSize);
+			
 			//xor     eax,eax
-			yield return new Instruction()
-			{
-				Code = Code.Xor_r32_rm32,
-				Op0Register = Register.EAX,
-				Op1Register = Register.EAX
-			};
+			yield return Instruction.Create(Code.Xor_r32_rm32, Register.EAX, Register.EAX);
 
-			//rep     stos dword ptr [rdi]
-			yield return new Instruction()
+			switch (operandSize)
 			{
-				Code = operandSize switch
-				{
-					1 => Code.Stosb_m8_AL,
-					2 => Code.Stosw_m16_AX,
-					4 => Code.Stosd_m32_EAX,
-					8 => Code.Stosq_m64_RAX,
-					_ => throw new InvalidOperationException()
-				},
-				HasRepePrefix = true,
-				Op0Kind = OpKind.MemoryESRDI,
-				Op0Register = Register.RDI,
-				Op1Register = operandSize switch
-				{
-					1 => Register.AL,
-					2 => Register.AX,
-					4 => Register.EAX,
-					8 => Register.RAX,
-					_ => throw new InvalidOperationException()
-				}
-			};
-
+				case 1:
+					yield return Instruction.CreateStosb(64, RepPrefixKind.Repe);
+					break;
+				case 2:
+					yield return Instruction.CreateStosw(64, RepPrefixKind.Repe);
+					break;
+				case 4:
+					yield return Instruction.CreateStosd(64, RepPrefixKind.Repe);
+					break;
+				case 8:
+					yield return Instruction.CreateStosq(64, RepPrefixKind.Repe);
+					break;
+				default:
+					throw new InvalidOperationException();
+			}
 		}
-
-
 	}
 }
